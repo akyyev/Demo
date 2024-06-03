e3222910790ee33feda85a34983fb747d1392ae6
bMac <bmac@bMacs-MacBook-Pro.local>
2024-06-02 19:17:24 -0500

    BigInteger to accommadate with extremely large numbers

--- src/main/java/data/algorithms/recursions/Recursion.java
+++ src/main/java/data/algorithms/recursions/Recursion.java
@@ -1,22 +1,23 @@
 package data.algorithms.recursions;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Recursion {
 
-    private Map<Integer, Integer> cache = new HashMap<>();
+    private Map<Integer, BigInteger> cache = new HashMap<>();
     
-    public int getFib(int order) {
+    public BigInteger getFib(int order) {
         // index: 0, 1, 2, 3, 4, 5, 6, 7,  ...
         // Fibs : 0, 1, 1, 2, 3, 5, 8, 13, ...
         // f(n) = f(n-1)+f(n-2)
         // base condition to terminate recursive calls
        if(cache.containsKey(order)) return cache.get(order);
 
-        if(order==0) return 0;
-        if(order==1) return 1;
-        int result = getFib(order-1) + getFib(order-2);
+        if(order==0) return BigInteger.ZERO;
+        if(order==1) return BigInteger.ONE;
+        BigInteger result = getFib(order-1).add(getFib(order-2));
         cache.put(order, result);
         return result;
     }
@@ -30,7 +31,7 @@ public class Recursion {
 
 class TestApp {
     public static void main(String[] args) {
-        System.out.println("Recursion.getFib(1000) = " + new Recursion().getFib(1000)); // 1556111435 with memoization using HashMap
+        System.out.println("Recursion.getFib(50) = " + new Recursion().getFib(50)); // 12586269025 with memoization using HashMap
         System.out.println("Recursion.getFactorial(5) = " + Recursion.getFactorial(5)); // 120
     }
 }
